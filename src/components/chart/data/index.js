7c7495c9ade40a7720a0a1f53cea4ec9d6dcdf4d
mluena <maluenarod@gmail.com>
2023-03-06 12:37:16 +0100

    governance chart real data

--- src/components/chart/data/index.js
+++ src/components/chart/data/index.js
@@ -27,10 +27,40 @@ export const dataArea = [
 ];
 
 export const dataRadialAfrica = [
-  { shortLabel: 'GE', label: 'Government effectiveness', africa: 42, rwanda: 62, labelValue: 50 },
-  { shortLabel: 'PS', label: 'Political stability', africa: 39, rwanda: 58, labelValue: 47 },
-  { shortLabel: 'VA', label: 'Voice and accountability', africa: 38, rwanda: 60, labelValue: 46 },
-  { shortLabel: 'CC', label: 'Control of corruption', africa: 39, rwanda: 59, labelValue: 47 },
-  { shortLabel: 'RL', label: 'Rule of Law', africa: 35, rwanda: 59, labelValue: 43 },
-  { shortLabel: 'RQ', label: 'Regulatory quality', africa: 40, rwanda: 58, labelValue: 48 },
+  {
+    shortLabel: 'GE',
+    label: 'Government effectiveness',
+    africa: 34.7080387753359,
+    rwanda: 56.5628062583581,
+  },
+  {
+    shortLabel: 'RQ',
+    label: 'Regulatory quality',
+    africa: 34.6524419806273,
+    rwanda: 52.613942548939,
+  },
+  {
+    shortLabel: 'RL',
+    label: 'Rule of Law',
+    africa: 37.0427837119317,
+    rwanda: 57.3551796524691,
+  },
+  {
+    shortLabel: 'CC',
+    label: 'Control of corruption',
+    africa: 28.7394386236532,
+    rwanda: 57.7649469607429,
+  },
+  {
+    shortLabel: 'VA',
+    label: 'Voice and accountability',
+    africa: 38.8746331944417,
+    rwanda: 29.114049106331,
+  },
+  {
+    shortLabel: 'PS',
+    label: 'Political stability',
+    africa: 43.3513483714081,
+    rwanda: 62.4804862546967,
+  },
 ];
--- src/components/chart/radial/component.tsx
+++ src/components/chart/radial/component.tsx
@@ -20,7 +20,6 @@ type RadialData = {
   label: string;
   africa: number;
   rwanda: number;
-  labelValue: number;
 };
 
 interface ChartDataTypes {
@@ -44,31 +43,36 @@ const Chart = ({
   const yMax = height - margin.top - margin.bottom;
   const radius = Math.min(xMax, yMax) / 1.45;
   const radialScale = scaleLinear({
-    domain: [0, 60],
+    domain: [0, 62],
     range: [0, innerHeight / 2.5],
   });
 
   const genPoints = (length: number, radius: number) => {
     const step = (Math.PI * 2) / length;
     return [...new Array(length)].map((_, i) => ({
-      rwandaX: ((radius * data[i].africa) / 100) * Math.sin(i * step + 15),
-      rwandaY: ((radius * data[i].africa) / 100) * Math.cos(i * step + 15),
-      africaX: ((radius * data[i].rwanda) / 100) * Math.sin(i * step + 11),
-      africaY: ((radius * data[i].rwanda) / 100) * Math.cos(i * step + 11),
-      labelX: ((radius * data[i].labelValue) / 100) * Math.sin(i * step + 15),
-      labelY: ((radius * data[i].labelValue) / 100) * Math.cos(i * step + 15),
+      rwandaX: ((radius * data[i].rwanda) / 100) * Math.sin(i * step + (step / 2) * 7.19),
+      rwandaY: ((radius * data[i].rwanda) / 100) * Math.cos(i * step + (step / 2) * 7.19),
+      africaX: ((radius * data[i].africa) / 100) * Math.sin(i * step + (step / 2) * 7.19 + 4),
+      africaY: ((radius * data[i].africa) / 100) * Math.cos(i * step + (step / 2) * 7.19 + 4),
+      labelX: ((radius * (data[i].rwanda + 7)) / 100) * Math.sin(i * step + (step / 2) * 7.19),
+      labelY: ((radius * (data[i].rwanda + 7)) / 100) * Math.cos(i * step + (step / 2) * 7.19),
       label: data[i].shortLabel,
     }));
   };
   const reverseYScale = radialScale.copy().range(radialScale.range().reverse());
 
   const points = genPoints(data.length, radius);
-
   return (
     <svg width={width} height={height}>
       <Group top={innerHeight / 2} left={innerWidth - innerWidth / 4}>
         <AnimatePresence>
-          <GridRadial scale={radialScale} stroke="#5BCEFB" numTicks={3} strokeWidth={1} />
+          <GridRadial
+            scale={radialScale}
+            stroke="#5BCEFB"
+            opacity={0.3}
+            numTicks={3}
+            strokeWidth={1}
+          />
           <motion.g
             initial={{ scale: 0 }}
             animate={{ scale: 1 }}
@@ -82,18 +86,16 @@ const Chart = ({
               scale={reverseYScale}
               numTicks={3}
               tickStroke="none"
-              tickLabelProps={() => ({
-                fontSize: 8,
-                fill: '#5BCEFB',
-                fillOpacity: 1,
-                textAnchor: 'middle',
-                dx: '1em',
-                dy: '-0.5em',
-                stroke: 'red',
-                strokeWidth: 0.5,
-                paintOrder: 'stroke',
-              })}
-              hideAxisLine
+              stroke="transparent"
+              tickComponent={(p) => {
+                return (
+                  <HtmlLabel {...p} x={p.x - 10} y={p.y - 12} showAnchorLine={false}>
+                    <div className="rounded-3xl border border-cyan-0 border-opacity-30 bg-cobalt-0 px-2 py-0.5 text-xs text-cyan-0">
+                      {p.formattedValue}
+                    </div>
+                  </HtmlLabel>
+                );
+              }}
             />
           </motion.g>
 
@@ -124,13 +126,15 @@ const Chart = ({
                 strokeLinecap="round"
               />
               <HtmlLabel
-                x={points[i].labelX}
-                y={points[i].labelY}
-                anchorLineStroke="transparent"
-                horizontalAnchor="end"
-                verticalAnchor="end"
+                x={points[i].labelX - 10}
+                y={points[i].labelY - 15}
+                showAnchorLine={false}
+                horizontalAnchor="middle"
+                verticalAnchor="middle"
               >
-                <p className="flex rounded-3xl bg-cobalt-0 text-xs text-cyan-0">{a.shortLabel}</p>
+                <p className="flex rounded-full bg-cobalt-0 p-2 text-xs text-cyan-0">
+                  {a.shortLabel}
+                </p>
               </HtmlLabel>
             </motion.g>
           ))}
